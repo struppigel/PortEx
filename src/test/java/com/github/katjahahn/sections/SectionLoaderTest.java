853ae0414e66661f79e0a6fe871e11c6510e08fa
Katja Hahn <kzeissle@imn.htwk-leipzig.de>
2014-04-01 20:14:28 +0200

    section table test

---
+++ src/test/java/com/github/katjahahn/sections/SectionLoaderTest.java
@@ -0,0 +1,70 @@
+package com.github.katjahahn.sections;
+
+import static org.testng.Assert.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.github.katjahahn.IOUtil.TestData;
+import com.github.katjahahn.PEData;
+import com.github.katjahahn.PELoaderTest;
+import com.github.katjahahn.sections.idata.DirectoryTableEntry;
+import com.github.katjahahn.sections.idata.ImportSection;
+
+public class SectionLoaderTest {
+	
+	private static final Logger logger = LogManager
+			.getLogger(SectionTableTest.class.getName());
+	private List<TestData> testdata;
+	private Map<String, PEData> pedata = new HashMap<>();
+
+	@BeforeClass
+	public void prepare() throws IOException {
+		testdata = PELoaderTest.getTestData();
+		pedata = PELoaderTest.getPEData();
+	}
+	
+	@Test
+	public void getDataDirEntryForKey() {
+		throw new RuntimeException("Test not implemented");
+	}
+
+	@Test
+	public void getSectionByRVA() {
+		throw new RuntimeException("Test not implemented");
+	}
+
+	@Test
+	public void loadImportSection() throws IOException {
+		for(PEData pedatum : pedata.values()) {
+			System.err.println("testing file " + pedatum.getFile().getName());
+			SectionLoader loader = new SectionLoader(pedatum);
+			ImportSection idata = loader.loadImportSection();
+			List<DirectoryTableEntry> directoryTable = idata.getDirectoryTable();
+			assertNotNull(directoryTable);
+			assertTrue(directoryTable.size() > 0);
+		}
+	}
+
+	@Test
+	public void loadResourceSection() {
+		throw new RuntimeException("Test not implemented");
+	}
+
+	@Test
+	public void loadSection() {
+		throw new RuntimeException("Test not implemented");
+	}
+
+	@Test
+	public void readBytesFor() {
+		throw new RuntimeException("Test not implemented");
+	}
+}
