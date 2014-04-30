0e78d463efefa687b926ea9b68e31eba2f81ede1
Katja Hahn <kzeissle@imn.htwk-leipzig.de>
2014-04-30 09:41:27 +0200

    flags for pe-anomaly scanning traits

--- src/main/java/com/github/katjahahn/tools/anomalies/PEAnomalyScanner.scala
+++ src/main/java/com/github/katjahahn/tools/anomalies/PEAnomalyScanner.scala
@@ -6,12 +6,23 @@ import com.github.katjahahn.PELoader
 import PartialFunction._
 
 class PEAnomalyScanner(data: PEData) extends AnomalyScanner(data) {
-
-  override def scanReport(): String = {
-    "scan report not implemented"
+  
+  /**
+   * Scans the PE and returns a report of the anomalies found.
+   * 
+   * @return a description string of the scan
+   */
+  override def scanReport: String = {
+    "scan report not implemented yet" //TODO
   }
 
-  override def scan(): List[Anomaly] = {
+  /**
+   * Scans the PE and returns a list of the anomalies found.
+   * Returns an empty list if no traits have been added.
+   * 
+   * @return list of anomalies found
+   */
+  override def scan: List[Anomaly] = {
     List[Anomaly]()
   }
 
@@ -19,16 +30,70 @@ class PEAnomalyScanner(data: PEData) extends AnomalyScanner(data) {
 
 object PEAnomalyScanner {
 
-  def apply(file: File): PEAnomalyScanner = {
+  /**
+   * Parses the given file and creates a PEAnomalyScanner instance that has the 
+   * scanning characteristics applied defined by the boolean scanning parameters.
+   * 
+   * @param file the pe file to scan for
+   * @param coffScanning adds COFF File Header scanning iff true
+   * @param optScanning adds Optional Header scanning iff true
+   * @param sectionTableScanning adds SectionTableScanning iff true
+   * @return a PEAnomalyScanner instance with the traits applied from the boolean values
+   */
+  def getInstance(file: File, coffScanning: Boolean, optScanning: Boolean, sectionTableScanning: Boolean): PEAnomalyScanner = {
     val data = PELoader.loadPE(file)
-    new PEAnomalyScanner(data) with COFFHeaderScanning
+    getInstance(data, coffScanning, optScanning, sectionTableScanning)
+  }
+  
+  /**
+   * Creates a PEAnomalyScanner instance that has the scanning characteristics 
+   * applied defined by the boolean scanning parameters.
+   * 
+   * @param data the PEData object created by the PELoader
+   * @param coffScanning adds COFF File Header scanning iff true
+   * @param optScanning adds Optional Header scanning iff true
+   * @param sectionTableScanning adds SectionTableScanning iff true
+   * @return a PEAnomalyScanner instance with the traits applied from the boolean values
+   */
+  def getInstance(data: PEData, coffScanning: Boolean, optScanning: Boolean, sectionTableScanning: Boolean): PEAnomalyScanner = {
+    val scanner = new PEAnomalyScanner(data) with SectionTableScanning
+    
+    //This is silly, but there is no choice as Scala doesn't allow dynamic mixins
+    /*three traits*/
+    if (coffScanning && optScanning && sectionTableScanning) {
+      new PEAnomalyScanner(data) with COFFHeaderScanning with OptionalHeaderScanning with SectionTableScanning
+    } 
+    
+    /*two traits*/ 
+    else if (coffScanning && optScanning) {
+      new PEAnomalyScanner(data) with COFFHeaderScanning with OptionalHeaderScanning
+    } else if (coffScanning && sectionTableScanning) {
+      new PEAnomalyScanner(data) with COFFHeaderScanning with SectionTableScanning
+    } else if (optScanning && sectionTableScanning) {
+      new PEAnomalyScanner(data) with OptionalHeaderScanning with SectionTableScanning
+    } 
+    
+    /*one trait*/ 
+    else if (coffScanning) {
+      new PEAnomalyScanner(data) with COFFHeaderScanning
+    } else if (optScanning) {
+      new PEAnomalyScanner(data) with OptionalHeaderScanning
+    } else if (sectionTableScanning) {
+      new PEAnomalyScanner(data) with SectionTableScanning
+    } 
+    
+    /*no traits*/ 
+    else {
+      new PEAnomalyScanner(data)
+    }
   }
 
   def main(args: Array[String]): Unit = {
     var counter = 0
     val files = new File("src/main/resources/x64viruses/").listFiles
     for (file <- files) {
-      val scanner = PEAnomalyScanner(file)
+      val data = PELoader.loadPE(file)
+      val scanner = new PEAnomalyScanner(data) with SectionTableScanning
       val list = scanner.scan
       if (list.size > 0 && !list.forall(_.isInstanceOf[NonDefaultAnomaly])) {
         counter += 1
--- src/main/java/com/github/katjahahn/tools/anomalies/SectionTableScanning.scala
+++ src/main/java/com/github/katjahahn/tools/anomalies/SectionTableScanning.scala
@@ -4,6 +4,13 @@ import scala.collection.mutable.ListBuffer
 
 trait SectionTableScanning extends AnomalyScanner {
 
+  //TODO ascending order of VAs
+  //multiple of FileAlignment SIZE_OF_RAW_DATA (for executable images only) and zero if only unitialized data
+  //multiple of FileAlignment POINTER_TO_RAW_DATA  and zero if only uninitialized data
+  //POINTER_TO_RELOC, NumberOfRelocations zero for executables
+  //PointerToLinenumbers, NumberOfLinenumbers zero for images (COFF debugging deprecated)
+  //Reserved section characteristic flags
+  
   abstract override def scan(): List[Anomaly] = {
     val anomalyList = ListBuffer[Anomaly]()
     super.scan ::: anomalyList.toList
