4131dcacab5cc1f0fa1a6a160366d29d52115f3f
Katja Hahn <kzeissle@imn.htwk-leipzig.de>
2014-05-13 16:12:40 +0200

    applied part of the suggestions by prof

--- src/test/java/com/github/katjahahn/PortexStats.java
+++ src/test/java/com/github/katjahahn/PortexStats.java
@@ -2,6 +2,11 @@ package com.github.katjahahn;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
+
+import com.github.katjahahn.optheader.DataDirEntry;
+import com.github.katjahahn.optheader.DataDirectoryKey;
+import com.github.katjahahn.sections.SectionLoader;
 
 public class PortexStats {
 
@@ -12,9 +17,49 @@ public class PortexStats {
 	public static void main(String[] args) throws IOException {
 		ableToLoad();
 	}
+	
+	public static int ableToLoadSections() {
+		int ableToLoad = 0;
+		int unableToLoad = 0;
+		int filesReadCounter = 0;
+		File folder = new File(PE_FOLDER);
+		File[] files = folder.listFiles();
+		for (File file : files) {
+			try {
+				PEData data = PELoader.loadPE(file);
+				SectionLoader loader = new SectionLoader(data);
+				Map<DataDirectoryKey, DataDirEntry> map = data.getOptionalHeader().getDataDirEntries();
+				if(map.containsKey(DataDirectoryKey.RESOURCE_TABLE)) {
+					loader.loadResourceSection();
+				}
+				if(map.containsKey(DataDirectoryKey.IMPORT_TABLE) ){
+					loader.loadImportSection();
+				}
+				if(map.containsKey(DataDirectoryKey.EXPORT_TABLE) ){
+					loader.loadExportSection();
+				}
+				ableToLoad++;
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				unableToLoad++;
+			}
+			filesReadCounter++;
+			if (filesReadCounter % 100 == 0) {
+				System.out.println("Files read: " + filesReadCounter);
+				System.out.println("Able to load: " + ableToLoad);
+				System.out.println("Unable to load: " + unableToLoad);
+				System.out.println();
+			}
+		}
+		System.out.println("Files read: " + filesReadCounter);
+		System.out.println("Able to load: " + ableToLoad);
+		System.out.println("Unable to load: " + unableToLoad);
+		return ableToLoad;
+	}
 
 	public static int ableToLoad() {
 		int ableToLoad = 0;
+		int unableToLoad = 0;
 		int filesReadCounter = 0;
 		File folder = new File(PE_FOLDER);
 		File[] files = folder.listFiles();
@@ -23,17 +68,21 @@ public class PortexStats {
 				PELoader.loadPE(file);
 				ableToLoad++;
 			} catch (Exception e) {
-				System.err.println(e.getMessage());
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+				unableToLoad++;
 			}
 			filesReadCounter++;
 			if (filesReadCounter % 100 == 0) {
 				System.out.println("Files read: " + filesReadCounter);
 				System.out.println("Able to load: " + ableToLoad);
+				System.out.println("Unable to load: " + unableToLoad);
 				System.out.println();
 			}
 		}
-		System.out.println("Able to load " + ableToLoad + "/"
-				+ files.length + " successfully.");
+		System.out.println("Files read: " + filesReadCounter);
+		System.out.println("Able to load: " + ableToLoad);
+		System.out.println("Unable to load: " + unableToLoad);
 		return ableToLoad;
 	}
 
