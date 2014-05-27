9bef531d7e74eceb3212713069f3980e3d254eb8
Katja Hahn <kzeissle@imn.htwk-leipzig.de>
2014-05-27 09:10:33 +0200

    cleaned up a bit

--- importtest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#creates import reports with pefile
-while read p; do
-  b=$(basename $p)
-  echo "creating src/main/resources/importreports/${b}.txt"
-  importtest.py $p 2> /dev/null > src/main/resources/importreports/${b}.txt
-done < filelist
--- overlay.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import pefile
-import sys
-
-pe =  pefile.PE(sys.argv[1], fast_load=False)
-print pe.get_overlay_data_start_offset()
