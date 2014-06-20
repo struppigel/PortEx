7d7866cc5af588fbf2e5ad5519a0f2e0adb36e52
Katja Hahn <kzeissle@imn.htwk-leipzig.de>
2014-06-20 14:26:28 +0200

    MalDet upload

--- src/main/java/com/github/katjahahn/tools/DetectionHeuristic.scala
+++ src/main/java/com/github/katjahahn/tools/DetectionHeuristic.scala
@@ -8,6 +8,10 @@ import scala.collection.JavaConverters._
 import com.github.katjahahn.tools.anomalies.AnomalySubType
 import Function.tupled
 import com.github.katjahahn.parser.IOUtil
+import scala.None
+import scala.None
+import scala.None
+import com.github.katjahahn.parser.PESignature
 
 //TODO remove dependend anomalies from /data/stats file
 //TODO test this more, also ask Schönherr
@@ -36,11 +40,107 @@ case class AnomalyProb(bad: Double, good: Double)
 
 object DetectionHeuristic {
 
+  val threshhold = 500
+  lazy val probabilities = readProbabilities()
+
+  private val version = """version: 0.1
+    |author: Katja Hahn
+    |last update: 20.Jun 2014""".stripMargin
+
+  private val title = """MalDet v0.1
+                        |-----------    
+                    |Please note: 
+                    |MalDet uses statistical information about file anomalies to assign a probability to a file for being malicious.
+                    |A probability of 50% means there is no knowledge about the file.
+                    |Files with 90% probability may still be non-malicious and vice versa for files with 10% probability.
+                    |MalDet is still experimental and not a substitute for any antivirus software!
+                    |MalDet is made with PortEx: https://github.com/katjahahn/PortEx
+                    |""".stripMargin
+
+  private val usage = """Usage: java -jar maldet.jar -f <pefile>
+                        |       java -jar maldet.jar -d <directory>
+    """.stripMargin
+
+  private type OptionMap = scala.collection.mutable.Map[Symbol, String]
+
   def main(args: Array[String]): Unit = {
-    val folder = new File("/home/deque/portextestfiles/goodfiles")
-    val threshholdA = 0.90
-    val threshholdB = 0.50
-    val threshholdC = 0.75
+    invokeCLI(args)
+  }
+
+  private def invokeCLI(args: Array[String]): Unit = {
+    val options = nextOption(scala.collection.mutable.Map(), args.toList)
+    println(title)
+    if (args.length == 0) {
+      println(usage)
+    } else if (options.contains('version)) {
+      println(version)
+    } else if (options.contains('inputfile)) {
+      try {
+        val filename = options('inputfile)
+        val file = new File(filename)
+        println("input file: " + filename)
+        if (!file.exists()) {
+          System.err.println("file doesn't exist!");
+        } else {
+          println("scanning file ...")
+          val prob = DetectionHeuristic(file).malwareProbability
+          println("malware probability: " + (prob * 100) + "%")
+          println("-done-")
+        }
+      } catch {
+        case e: Exception => System.err.println(e.getMessage());
+      }
+    } else if (options.contains('directory)) {
+      try {
+        val foldername = options('directory)
+        val folder = new File(foldername)
+        println("input folder: " + foldername)
+        if (!folder.exists()) {
+          System.err.println("folder doesn't exist!");
+        } else {
+          println("scanning files ...")
+          for (file <- folder.listFiles()) {
+            if (isPEFile(file)) {
+              val prob = DetectionHeuristic(file).malwareProbability
+              println(file.getName() + " malware probability: " + (prob * 100) + "%")
+            } else {
+              println(file.getName() + " is no PE file")
+            }
+          }
+          println("-done-")
+        }
+      } catch {
+        case e: Exception => System.err.println(e.getMessage());
+      }
+
+    } else {
+      println(usage)
+    }
+  }
+
+  private def isPEFile(file: File): Boolean = {
+    !file.isDirectory() && new PESignature(file).hasSignature()
+  }
+
+  private def nextOption(map: OptionMap, list: List[String]): OptionMap = {
+    list match {
+      case Nil => map
+      case "-d" :: value :: tail =>
+        nextOption(map += ('directory -> value), tail)
+      case "-v" :: tail =>
+        nextOption(map += ('version -> ""), tail)
+      case "-f" :: value :: tail =>
+        nextOption(map += ('inputfile -> value), tail)
+      case option :: tail =>
+        println("Unknown option " + option + "\n" + usage)
+        sys.exit(1)
+    }
+  }
+  private def testHeuristics(): Unit = {
+    val folder = new File("/home/deque/virusshare128/pe")
+    val threshholdA = 0.99999
+    val threshholdB = 0.99
+    val threshholdC = 0.95
     var malcounterA = 0
     var malcounterB = 0
     var malcounterC = 0
@@ -60,31 +160,31 @@ object DetectionHeuristic {
         }
         if (total % 1000 == 0) {
           println("files read: " + total)
-          println("malicious by threshhold 0.50: " + malcounterB + " ratio " + (malcounterB.toDouble / total.toDouble))
-          println("malicious by threshhold 0.75: " + malcounterC + " ratio " + (malcounterC.toDouble / total.toDouble))
-          println("malicious by threshhold 0.90: " + malcounterA + " ratio " + (malcounterA.toDouble / total.toDouble))
+          println("malicious by threshhold 0.99: " + malcounterB + " ratio " + (malcounterB.toDouble / total.toDouble))
+          println("malicious by threshhold 0.95: " + malcounterC + " ratio " + (malcounterC.toDouble / total.toDouble))
+          println("malicious by threshhold 0.999: " + malcounterA + " ratio " + (malcounterA.toDouble / total.toDouble))
         }
       } catch {
         case e: Exception => System.err.println(e.getMessage);
       }
     }
     println("files read: " + total)
-    println("malicious by threshhold 0.50: " + malcounterB + " ratio " + (malcounterB.toDouble / total.toDouble))
-    println("malicious by threshhold 0.75: " + malcounterC + " ratio " + (malcounterC.toDouble / total.toDouble))
-    println("malicious by threshhold 0.90: " + malcounterA + " ratio " + (malcounterA.toDouble / total.toDouble))
+    println("malicious by threshhold 0.95: " + malcounterB + " ratio " + (malcounterB.toDouble / total.toDouble))
+    println("malicious by threshhold 0.90: " + malcounterC + " ratio " + (malcounterC.toDouble / total.toDouble))
+    println("malicious by threshhold 0.999: " + malcounterA + " ratio " + (malcounterA.toDouble / total.toDouble))
   }
+
+  def newInstance(file: File): DetectionHeuristic = apply(file)
 
   def apply(file: File): DetectionHeuristic = {
     val data = PELoader.loadPE(file)
     val scanner = PEAnomalyScanner.newInstance(data)
     val list = scanner.getAnomalies.asScala.toList
-    val probabilities = readProbabilities()
     new DetectionHeuristic(list, probabilities)
   }
 
   private def clean(bad: Map[String, Array[String]],
     good: Map[String, Array[String]]): (Map[String, Double], Map[String, Double]) = {
-    val threshhold = 500
     val newBad = scala.collection.mutable.Map[String, Double]()
     val newGood = scala.collection.mutable.Map[String, Double]()
     for ((key, arr) <- bad) {
@@ -105,12 +205,22 @@ object DetectionHeuristic {
     val rawMalprobs = IOUtil.readMap("malwareanomalystats").asScala.toMap
     val rawGoodprobs = IOUtil.readMap("goodwareanomalystats").asScala.toMap
     val (malprobs, goodprobs) = clean(rawMalprobs, rawGoodprobs)
-    malprobs map tupled { (key: String, malicious: Double) =>
+    //    println("probs nr: " + malprobs.size)
+    //    println("cleaned: " + (rawMalprobs.size - malprobs.size))
+    //    println("cleaning threshhold: " + threshhold)
+    //    println()
+    (malprobs map tupled { (key: String, malicious: Double) =>
       val subtype = AnomalySubType.valueOf(key)
       val good = goodprobs.getOrElse(key, 0.5)
       val prob = AnomalyProb(malicious, good)
       (subtype, prob)
-    } //TODO include goodprobs that are not in malprobs
+    }).toMap ++
+      (goodprobs.filterNot(t => malprobs.contains(t._1)) map tupled { (key, good) =>
+        val subtype = AnomalySubType.valueOf(key)
+        val malicious = malprobs.getOrElse(key, 0.5)
+        val prob = AnomalyProb(malicious, good)
+        (subtype, prob)
+      }).toMap
   }
 
 }
