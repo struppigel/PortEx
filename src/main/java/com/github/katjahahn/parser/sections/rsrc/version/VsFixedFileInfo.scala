fdd8e25d7932accd1ff168bd72ec41b6dc137033
Katja Hahn <struppigel@googlemail.com>
2016-01-21 09:52:46 +0100

    copyright added

--- src/main/java/com/github/katjahahn/parser/sections/rsrc/version/VsFixedFileInfo.scala
+++ src/main/java/com/github/katjahahn/parser/sections/rsrc/version/VsFixedFileInfo.scala
@@ -1,6 +1,6 @@
 /**
  * *****************************************************************************
- * Copyright 2014 Katja Hahn
+ * Copyright 2015 Katja Hahn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
