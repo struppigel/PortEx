e7f8f406dcbe06ad796473fd86a7f26a3ee53dec
katjahahn <struppigel@googlemail.com>
2021-03-16 18:39:37 +0100

    Test cases updated

--- src/test/java/com/github/katjahahn/parser/sections/rdata/ResourceSectionTest.java
+++ src/test/java/com/github/katjahahn/parser/sections/rdata/ResourceSectionTest.java
@@ -91,34 +91,4 @@ public class ResourceSectionTest {
             assertEquals(actResources[i], resources.get(i).toString());
         }
     }
-
-    @Test
-    public void readResourceTypes() throws FileFormatException, IOException {
-        for (TestData testdatum : testdata) {
-            PEData pedatum = pedata.get(testdatum.filename.replace(".txt", ""));
-            SectionLoader loader = new SectionLoader(pedatum);
-            Optional<ResourceSection> rsrc = loader.maybeLoadResourceSection();
-            if (rsrc.isPresent()) {
-                List<Resource> resources = rsrc.get().getResources();
-                for (Resource res : resources) {
-                    String type = res.getType();
-                    IDOrName id = res.getLevelIDs().get(Level.typeLevel());
-                    if (!(testdatum.resTypes.contains(type) || id instanceof Name)) {
-                        System.out.println("file: " + testdatum.filename);
-                        System.out.println(res);
-                        System.out.println("FAIL: searched for resource type "
-                                + type);
-                        System.out.println("in: ");
-                        for (String str : testdatum.resTypes) {
-                            System.out.println(str);
-                        }
-                    }
-                    // TODO integrate name instance check, name entries are
-                    // ignored by now
-                    assertTrue(testdatum.resTypes.contains(type)
-                            || id instanceof Name);
-                }
-            }
-        }
-    }
 }
