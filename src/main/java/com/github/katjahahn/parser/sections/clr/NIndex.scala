beb986b2a94745238029997f6e5b4a7442ac83a8
struppigel <Karsten.Hahn@gdata.de>
2022-08-24 12:34:25 +0200

    new PortexAnalyzer version

---
+++ src/main/java/com/github/katjahahn/parser/sections/clr/NIndex.scala
@@ -0,0 +1,5 @@
+package com.github.katjahahn.parser.sections.clr
+
+class NIndex {
+
+}
---
+++ src/main/java/com/github/katjahahn/parser/sections/clr/StringsHeap.scala
@@ -0,0 +1,44 @@
+/**
+ * *****************************************************************************
+ * Copyright 2022 Karsten Hahn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ****************************************************************************
+ */
+package com.github.katjahahn.parser.sections.clr
+
+import com.github.katjahahn.parser.MemoryMappedPE
+
+class StringHeap(val strings : Array[String]) {
+
+  /**
+   * Retrieve string at the given index, starting with index 1 as it is customary for .NET table indices
+   * @param index
+   * @return string at index
+   */
+  def get(index : Int): String = {
+    assert(index > 0)
+    assert(index < strings.size)
+    strings(index - 1)
+  }
+
+}
+
+object StringHeap {
+
+  def apply(size: Long, offset : Long, mmbytes: MemoryMappedPE): StringHeap = {
+    val bytes = mmbytes.slice(offset, offset + size)
+    val strings = new String(bytes, "UTF-8").split("\0")
+    new StringHeap(strings)
+  }
+}
