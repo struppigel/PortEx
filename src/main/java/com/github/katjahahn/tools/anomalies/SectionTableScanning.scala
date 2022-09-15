27c124b3331646f0f2966ede18523d058101c432
struppigel <Karsten.Hahn@gdata.de>
2022-09-15 13:58:40 +0200

    Do not care about zero sized sections for section equality and overlap anomalies

--- src/main/java/com/github/katjahahn/tools/anomalies/SectionTableScanning.scala
+++ src/main/java/com/github/katjahahn/tools/anomalies/SectionTableScanning.scala
@@ -403,10 +403,10 @@ trait SectionTableScanning extends AnomalyScanner {
    * @return anomaly list
    */
   private def checkOverlappingOrShuffledSections(): List[Anomaly] = {
+    // Checks if the ranges overlap. Sections with zero size cannot overlap
     def overlaps(t1: SectionRange, t2: SectionRange): Boolean =
-      !(((t1._1 < t2._1) && (t1._2 <= t2._1)) || ((t2._1 < t1._1) && (t2._2 <= t1._1)))
-
-    def notEmpty(range: SectionRange): Boolean = range._1 != range._2
+      !(((t1._1 < t2._1) && (t1._2 <= t2._1)) || ((t2._1 < t1._1) && (t2._2 <= t1._1)) ||  zeroSize(t1) ||  zeroSize(t2))
+    def zeroSize(range: SectionRange): Boolean = range._1 == range._2
 
     val anomalyList = ListBuffer[Anomaly]()
     val sectionTable = data.getSectionTable
@@ -420,19 +420,19 @@ trait SectionTableScanning extends AnomalyScanner {
         val range2 = physicalSectionRange(sec)
         val vrange2 = virtualSectionRange(sec)
         val locations = List(range1, range2).map(r => new PhysicalLocation(r._1, r._2 - r._1))
-        //ignore empty sections for shuffle analysis, these get their own anomaly
-        if (range1._1 > range2._1 && notEmpty(range1) && notEmpty(range2)) {
+        // ignore zero sized sections for shuffle analysis, these get their own anomaly
+        if (range1._1 > range2._1 && !zeroSize(range1) && !zeroSize(range2)) {
           val description = s"Physically shuffled sections: section ${section.getNumber} has range 0x${range1._1.toHexString}--0x${range1._2.toHexString}, section ${sec.getNumber} has range 0x${range2._1.toHexString}--0x${range2._2.toHexString}"
           anomalyList += StructureAnomaly(PEStructureKey.SECTION, description, PHYSICALLY_SHUFFLED_SEC, locations)
         }
-        if (range1 == range2) {
+        if (range1 == range2 && !zeroSize(range1)) {
           val description = s"Section ${section.getNumber} with name $sectionName (range: 0x${range1._1.toHexString}--0x${range1._2.toHexString}) has same physical location as section ${sec.getNumber} with name ${sec.getName}"
           anomalyList += StructureAnomaly(PEStructureKey.SECTION, description, PHYSICALLY_DUPLICATED_SEC, locations)
         } else if (overlaps(range2, range1)) {
           val description = s"Section ${section.getNumber} with name $sectionName (range: 0x${range1._1.toHexString}--0x${range1._2.toHexString}) physically overlaps with section ${sec.getName} with number ${sec.getNumber} (range: 0x${range2._1.toHexString}--0x${range2._2.toHexString})"
           anomalyList += StructureAnomaly(PEStructureKey.SECTION, description, PHYSICALLY_OVERLAPPING_SEC, locations)
         }
-        if (vrange1 == vrange2) {
+        if (vrange1 == vrange2 && !zeroSize(range1)) {
           val description = s"Section ${section.getNumber} with name $sectionName (range: 0x${vrange1._1.toHexString}--0x${vrange1._2.toHexString}) has same virtual location as section ${sec.getName} with number ${sec.getNumber} (range: 0x${vrange2._1.toHexString}--0x${vrange2._2.toHexString})"
           anomalyList += StructureAnomaly(PEStructureKey.SECTION, description, VIRTUALLY_DUPLICATED_SEC, locations)
         } else if (overlaps(vrange1, vrange2)) {
